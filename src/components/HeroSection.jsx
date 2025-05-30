bc1f6e5591051800d5b9efe1d4f99580fe8f7d64
srun.veng <srun.veng@wingbank.com.kh>
2025-05-30 10:32:03 +0700

    fixed-4

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -32,7 +32,7 @@ export const HeroSection = () => {
                 View My Work
               </a>
               <a
-                  href="public/resume/Veng Srun.pdf"
+                  href="/resume/Veng Srun.pdf"
                   download
                   className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
